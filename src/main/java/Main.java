7b558a9a77e89d860a3af2c60db7adb34d8e5caf
Victor Fabris <victor.fabris.santos@escola.pr.gov.br>
2024-07-04 10:58:30 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Digite seu nome:");
+    String nome = sc.nextLine();
+    System.out.println("Digite seu sobrenome:");
+    String sobrenome = sc.nextLine();
+    System.out.println("Digite sua idade:");
+    int idade = sc.nextInt();
+    System.out.println("Digite seu peso:");
+    double peso = sc.nextDouble();
+    System.out.println("Digite sua altura:");
+    double altura = sc.nextDouble();
+    System.out.println("Digite seu sexo:");
+    String sexo = sc.nextLine();
+
+ 
+  }
+
+}
